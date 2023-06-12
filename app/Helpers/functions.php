c323777c2f76762cc40c950163623d528b7b9fdf
Toni Kunštek <tony.kunstek@gmail.com>
2023-06-12 14:51:50 +0200

    Updated gitignore: ignoring users.json, added Helpers/functions.php

---
+++ app/Helpers/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function formatTimeAgo($dateTime)
+{
+    $now = now();
+    $diff = $dateTime->diffInSeconds($now);
+
+    if ($diff < 60) {
+        return $diff . ' seconds ago';
+    } elseif ($diff < 3600) {
+        $minutes = floor($diff / 60);
+        return $minutes . ' minutes ago';
+    } elseif ($diff < 86400) {
+        $hours = floor($diff / 3600);
+        return $hours . ' hours ago';
+    } else {
+        $days = floor($diff / 86400);
+        return $days . ' days ago';
+    }
+}
+?>
--- resources/views/partials/posts_list.blade.php
+++ resources/views/partials/posts_list.blade.php
@@ -5,25 +5,6 @@ use Illuminate\Database\Query\Builder;
 
     $posts = Post::orderByDesc('created_at')->get();
 
-    function formatTimeAgo($dateTime)
-    {
-        $now = now();
-        $diff = $dateTime->diffInSeconds($now);
-
-        if ($diff < 60) {
-            return $diff . ' seconds ago';
-        } elseif ($diff < 3600) {
-            $minutes = floor($diff / 60);
-            return $minutes . ' minutes ago';
-        } elseif ($diff < 86400) {
-            $hours = floor($diff / 3600);
-            return $hours . ' hours ago';
-        } else {
-            $days = floor($diff / 86400);
-            return $days . ' days ago';
-        }
-    }
-
     function userIsAFriend($userId) {
         // return in_array($userId, $friends);
         $friends = session('friends', []);
